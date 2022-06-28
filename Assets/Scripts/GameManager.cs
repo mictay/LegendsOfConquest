8706e2c33b8329319b35d0e522ead7d6ca21e15d
Michael <michael.taylor@celebrationsoftware.com>
2022-06-28 12:24:48 -0400

    Completed Section 6-45 Character Info

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance { get; private set; }
+
+    [SerializeField] PlayerStats[] playerStats;
+
+    private void Awake()
+    {
+        Debug.Log("GameManager Awake() called");
+
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        //Look around and gather every game object that has a player stat
+        playerStats = FindObjectsOfType<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
